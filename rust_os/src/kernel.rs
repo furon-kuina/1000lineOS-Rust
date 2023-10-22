0e1627b6c899bfc24a22d622c41481dce5965444
Furon-Kuina <tenma.edamura@gmail.com>
2023-10-22 19:25:26 +0900

    chapter 6: implement sbi_call and putchar

--- rust_os/src/kernel.rs
+++ rust_os/src/kernel.rs
@@ -3,6 +3,8 @@
 
 use core::arch::asm;
 use core::panic::PanicInfo;
+use rustsbi;
+use rustsbi::spec::binary::SbiRet;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -27,17 +29,19 @@ fn memset(buf: *mut u8, c: u8, n: usize) {
 
 #[allow(dead_code)]
 fn kernel_main() {
-    let bss_size = unsafe { &__bss_end as *const _ as usize - &__bss as *const _ as usize };
-    unsafe { memset(&__bss as *const _ as *mut _, 0, bss_size) };
+    let s = "\n\nHello, world!\n";
+    for ch in s.chars() {
+        putchar(ch as u8)
+    }
 
-    // 無限ループ
-    loop {}
+    loop {
+        unsafe { asm!("wfi") }
+    }
 }
 
 #[no_mangle]
 #[link_section = ".text.boot"]
 pub unsafe extern "C" fn boot() -> ! {
-    // スタックポインタを初期化
     asm!(
         "mv sp, {stack_top}\n
         j {kernel_main}\n",
@@ -45,4 +49,39 @@ pub unsafe extern "C" fn boot() -> ! {
         kernel_main = sym kernel_main,
     );
     loop {}
+}
+
+#[inline(always)]
+fn sbi_call(
+    arg0: u32,
+    arg1: u32,
+    arg2: u32,
+    arg3: u32,
+    arg4: u32,
+    arg5: u32,
+    fid: u32,
+    eid: u32,
+) -> SbiRet {
+    let (error, value);
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") arg0,
+            in("a1") arg1,
+            in("a2") arg2,
+            in("a3") arg3,
+            in("a4") arg4,
+            in("a5") arg5,
+            in("a6") fid,
+            in("a7") eid,
+            lateout("a0") error,
+            lateout("a1") value,
+            options(nostack, nomem)
+        );
+    }
+    SbiRet { error, value }
+}
+
+fn putchar(ch: u8) {
+    sbi_call(ch as u32, 0, 0, 0, 0, 0, 0, 1);
 }
